7bbf02407490f67c090fd8ac971b6a186b30a22a
CuriousGeorge <george.dorrinton2@gmail.com>
2023-08-21 17:41:32 +1200

    Initial Commit

---
+++ Shared/CarPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPediaApp.swift
+//  Shared
+//
+//  Created by George Dorrington on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CarPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
